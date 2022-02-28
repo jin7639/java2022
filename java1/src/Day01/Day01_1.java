a1b11262cf7f81b5ab586ce6951d50ac9c38a7cf
504 <504@DESKTOP-83O6Q98>
2022-02-28 17:53:51 +0900

    2022-02-28

---
+++ java1/src/Day01/Day01_1.java
@@ -0,0 +1,29 @@
+package Day01; //패키지명 [ 소스파일들의 폴더 ]
+// 확대/축소 : ctrl + shift + +/-
+// 주석
+	// // 한 줄 주석
+	// /* 여러줄 주석 */
+public class Day01_1 { //class start
+	//public: 접근제한자 -> 전범위
+		//class: 클래스 선언
+			//Day01_1 : 클래스 이름
+	// 모든 곳에서 사용하는 Day01_1 이라는 이름을 갖는 클래스 선언
+		//{ : 시작,	} : 끝
+	// 1. class 안에서 코드 작성 [ 클래스 밖에서 작성 불가]
+		//1. 자바는 100% 객체지향 
+	//2. 실행코드 main함수
+		//main ->ctrl +space
+	public static void main(String[] args) {
+		//public : 접근제한자
+			//static : 정적	
+				//void : 반환타입 없음
+					//main 함수 : 메인 스레드를 가지고 있는 함수 [필수]
+					//스레드 : 코드를 읽어주는 흐름
+		
+		// *main 함수 { }안에 작성된 코드는 실행된다.
+		
+		// syso -> ctrl + space bar
+		System.out.println("java 출력"); //고급언어 [사람이 이해하는 언어]
+		//컴파일 ctrl + F11
+	}// main end
+} //class end
