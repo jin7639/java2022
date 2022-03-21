7bb0abc9232ea48e334908767f8c33e97ec55396
Jinny <98166529+jin7639@users.noreply.github.com>
2022-03-21 17:52:59 +0900

    Day14

---
+++ java1/src/Day14/Sound.java
@@ -0,0 +1,32 @@
+package Day14;
+
+public class Sound extends Thread{
+
+	static boolean stop = true;	//실행 여부 [true 시작 false 종료]
+	
+	public void soundstop (boolean stop) {
+		this.stop = stop;
+		if (this.stop) {
+			System.out.println("소리 재생 시작~~~");
+		}else {
+			System.out.println("소리 끄기!!");
+		}
+	}
+	
+	
+	@Override
+	public void run() {
+	
+		while(stop) { // stop 변수가 true이면 무한루프 [ 종료조건 : stop 변수가 false ]
+			System.out.println("소리 재생중~~~~~");
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+	}
+	
+	
+}
---
+++ java1/src/Day14/회원.java
@@ -0,0 +1,15 @@
+package Day14;
+
+public class 회원 {
+
+	String  id;
+	String password;;
+	String name;
+	int point;
+	
+	public 회원() {}
+	
+	
+	
+	
+}
