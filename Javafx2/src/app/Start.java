6e246f9f39ace3aa74e419c62fefbcf21cb7f7fb
Jinny <[98166529+jin7639@users.noreply.github.com](mailto:98166529+jin7639@users.noreply.github.com)>
2022-03-30 13:20:01 +0900

    JAVAFX

---
+++ Javafx2/src/app/Start.java
@@ -0,0 +1,40 @@
+package app;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Start extends Application{
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Parent parent = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+		
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		//3.스테이지 로고 설정
+			//1.이미지 불러오기
+		Image image = new Image("/img/stage.jpg");
+			//절대 경로 vs 상대경로
+			//절대 경로 : 모든 경로
+				//C:\Users\504\git\java2022\Javafx2\src\img\파일명.확장자
+			//상대 경로 : 현 위치[현재 프로젝트 기준 src] 기준 경로
+				// /img/파일명.확장자
+			//2.스테이지 설정
+		stage.setResizable(false); //스테이지 크기 고정
+		stage.getIcons().add(image); //스테이지 아이콘 설정
+		
+		stage.setTitle("EZEN"); //스테이지 창 이름
+		stage.show(); //1.스테이지 열기
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	
+}
---
+++ Javafx2/src/controller/Main.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+public class Main implements Initializable {
+
+	
+	 @FXML
+    private BorderPane borderpane; //씬빌더에서 만든 컨테이너 객체
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("메인 뷰가 실행되었습니다.");	
+		//1.
+		loadpage("/view/login");
+	
+	}
+	public void loadpage(String page) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource(page+".fxml"));
+			borderpane.setCenter(parent); //컨테이너 (fxml) 가운데에 페이지 넣기
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+}
